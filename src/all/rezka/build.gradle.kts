b4fb72c21b7fffd109053e087144ff97f3daca11
Katari extensions <katari.media.app@gmail.com>
2026-07-14 10:20:39 +0200

    fix(rezka): merge episode catalogs across dubs

--- src/all/rezka/build.gradle.kts
+++ src/all/rezka/build.gradle.kts
@@ -11,8 +11,8 @@ configureSharedExtensionModule(project)
 
 android {
     defaultConfig {
-        versionCode = 13
-        versionName = "2.0.3"
+        versionCode = 14
+        versionName = "2.0.4"
         applicationId = "eu.kanade.tachiyomi.extension.all.rezka"
 
         manifestPlaceholders += mapOf(
