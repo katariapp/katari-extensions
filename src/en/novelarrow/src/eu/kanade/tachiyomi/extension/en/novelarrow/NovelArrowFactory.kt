9079e94b59f2f993f1edc56ff635dfbe664467e0
Katari extensions <katari.media.app@gmail.com>
2026-07-17 09:32:09 +0200

    (chore): remove unreliable NovelArrow extension

--- src/en/novelarrow/src/eu/kanade/tachiyomi/extension/en/novelarrow/NovelArrowFactory.kt
+++
@@ -1,8 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.novelarrow
-
-import eu.kanade.tachiyomi.source.entry.EntrySourceFactory
-import eu.kanade.tachiyomi.source.entry.UnifiedSource
-
-class NovelArrowFactory : EntrySourceFactory {
-    override fun createSources(): List<UnifiedSource> = listOf(NovelArrowSource())
-}
